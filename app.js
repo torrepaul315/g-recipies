c4b82a552cb3326309bc0ace81b04194e9bc07bb
Paul Torres <torrepaul@gmail.com>
2017-03-15 09:36:56 -0600

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 // var users = require('./routes/users');
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
